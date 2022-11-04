6d8f6eb22b73ed23e35f86a9ca146851062053b3
kody524 <kody524@icloud.com>
2022-11-03 20:16:08 -0500

    updated code

--- app.js
+++ app.js
@@ -1,31 +1,51 @@
 const introPage = document.getElementById("front-page");
 const playerVsPlayer = document.getElementById("pvp");
 const vsComputer = document.getElementById("vscomputer");
+const divForButton = document.getElementById("buttonId");
+const randomButton = document.getElementById("random");
+const player1 = document.getElementById("player1");
+const player2 = document.getElementById("player2");
+const table = document.getElementById("tables");
 
+const gameState = {
+  players: ['x', 'o'],
+  board: [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null]
+  ]
+}
 
-playerVsPlayer.addEventListener("click",function(){
-    introPage.className= "frontpage-hide";
-    names()
-})
+playerVsPlayer.addEventListener("click", function () {
+  introPage.className = "frontpage-hide";
+  divForButton.className = "button"
+  names();
+});
 
-
-
-
-
-
-
-
-function names(){
-    let firstPlayerName = prompt("First Player Name","Full-Name");
-    let secondPlayerName = prompt("Second Player Name","Full-Name");
-    if(firstPlayerName != null){
-        document.getElementById("playerone").innerHTML = "Player 1:" + firstPlayerName;
+function names() {
+  let firstPlayerName = prompt("First Player Name");
+  let secondPlayerName = prompt("Second Player Name");
+  if (firstPlayerName != null) {
+    player1.innerHTML = "X:" + firstPlayerName; 
+  }
+  if (secondPlayerName != null) {
+    player2.innerHTML = "O:" + secondPlayerName;
+      
+  }
+}
+function randomTurn(){
+    let number = Math.floor(Math.random()*2);
+    console.log(number)
+    if(number===1){
+        player1.className = "playeroneturn"
     }else{
-        document.getElementById("playerone").innerHTML = "Player 1: No Name";
-    }
-    if(secondPlayerName != null){
-        document.getElementById("playertwo").innerHTML = "Player 2:" + secondPlayerName;
-    }else{
-        document.getElementById("playertwo").innerHTML = "Player 2: No Name"
+        player2.className = "playertwoturn"
     }
 }
+function chooseBox(event){
+  let target = event.target;
+
+}
+table.addEventListener("click",chooseBox)
+
+randomButton.addEventListener("click",randomTurn)
